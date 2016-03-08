f77b5545807285d7bb824376f8eb7f8705a3823f
Jesus <Jesus@DESKTOP-JB58JJB>
2016-03-08 12:04:00 -0600

    First commit

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -7,6 +7,7 @@ $this->breadcrumbs=array(
 );
 ?>
 <h1>About</h1>
-
+<br>
+<p>Jesus Salazar Maldonado - @iSalazarmx</p>
 <p>This is a "static" page. You may change the content of this page
 by updating the file <code><?php echo __FILE__; ?></code>.</p>
